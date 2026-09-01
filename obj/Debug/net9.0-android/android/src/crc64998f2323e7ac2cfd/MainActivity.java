05dcdbe4b87988798b57129bc7bd9ed32bf23e26
Maurice Ruffin <Tysons3x3@gmail.com>
2026-09-01 18:29:31 -0400

    finished

---
+++ obj/Debug/net9.0-android/android/src/crc64998f2323e7ac2cfd/MainActivity.java
@@ -0,0 +1,46 @@
+package crc64998f2323e7ac2cfd;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("MauiHelloApp.MainActivity, MauiHelloApp", MainActivity.class, __md_methods);
+	}
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("MauiHelloApp.MainActivity, MauiHelloApp", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("MauiHelloApp.MainActivity, MauiHelloApp", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
